1a0d961366adbf8406e71ad9f788616269ee396d
Baybars87 <emadbeebrse@gmail.com>
2019-11-28 12:41:54 +0200

    3 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HelloComponent } from './hello.component';
 
 import { NgRedux, NgReduxModule } from '@angular-redux/store';
 import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { TodoOverviewComponent } from './todo-overview/todo-overview.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   imports: [ 
@@ -14,7 +16,7 @@ import { IAppState, rootReducer, INITIAL_STATE } from './store';
       FormsModule,
       NgReduxModule 
     ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TodoOverviewComponent, TodoListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
